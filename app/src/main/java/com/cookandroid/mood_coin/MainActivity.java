92e9e5a5e8e726424c3a40320f3f569aac03d4a7
hangyeol0531 <41174265+hangyeol0531@users.noreply.github.com>
2019-12-19 14:11:03 +0900

    ㅇㅇㅇㅇ

--- app/src/main/java/com/cookandroid/mood_coin/MainActivity.java
+++ app/src/main/java/com/cookandroid/mood_coin/MainActivity.java
@@ -2,12 +2,27 @@ package com.cookandroid.mood_coin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn1;
+    TextView tv1;
+    String msg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1 = (Button)findViewById(R.id.bt1);
+        tv1 = (TextView)findViewById(R.id.test);
+        msg = "11";
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv1.setText(msg);
+            }
+        });
     }
 }
