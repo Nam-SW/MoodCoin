847506c8e9ab82ab870605baa8064a4ea7e186dd
yoongkim <dl2qja@gmail.com>
2019-12-23 08:53:09 +0900

    firebase 연결함

--- app/src/main/java/com/cookandroid/mood_coin/MainActivity.java
+++ app/src/main/java/com/cookandroid/mood_coin/MainActivity.java
@@ -2,10 +2,11 @@ package com.cookandroid.mood_coin;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.json.JSONObject;
 
