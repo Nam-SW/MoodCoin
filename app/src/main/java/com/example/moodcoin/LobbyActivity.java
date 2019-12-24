0a1ca4700e0353fac39e4677d51df9ab48257885
yoongkim <dl2qja@gmail.com>
2019-12-24 16:09:11 +0900

    firebase 연동

--- app/src/main/java/com/example/moodcoin/LobbyActivity.java
+++ app/src/main/java/com/example/moodcoin/LobbyActivity.java
@@ -4,56 +4,96 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class LobbyActivity extends Activity {
 
     String happy, fireangry, disappear, sad, angry;
-    private DatabaseReference mDB_ref;
-    String id = getIntent().getStringExtra("id");
-    String today = getIntent().getStringExtra("today");
+    DatabaseReference mDB_ref ;
+    String id;
+    String today;
     TextView eh, efa, eda, es, ea;
+    HashMap<String, Object> childUpdates = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
 
-        mDB_ref = FirebaseDatabase.getInstance().getReference().child(id).child(today);
-        mDB_ref.addValueEventListener(Listener);
+        childUpdates = new HashMap<>();
+        id = getIntent().getStringExtra("id");
+        today = getIntent().getStringExtra("today");
+        mDB_ref = FirebaseDatabase.getInstance().getReference().child(id);
+
+        isExist();
+
         eh = (TextView) findViewById(R.id.happy);
         efa = (TextView) findViewById(R.id.fireangry);
-        eda = (TextView)findViewById(R.id.disappear);
-        es = (TextView)findViewById(R.id.sad);
-        ea = (TextView)findViewById(R.id.angry);
+        eda = (TextView) findViewById(R.id.disappear);
+        es = (TextView) findViewById(R.id.sad);
+        ea = (TextView) findViewById(R.id.angry);
+
+
+
+        mDB_ref.addValueEventListener(Listener);
+
+    }
+
+    private void isExist() {
+
     }
 
     ValueEventListener Listener = new ValueEventListener() {
+
         @Override
         public void onDataChange(DataSnapshot dataSnapshot) {
-            happy = dataSnapshot.child("기쁨").getValue().toString();
-            fireangry = dataSnapshot.child("분노").getValue().toString();
-            disappear = dataSnapshot.child("불안").getValue().toString();
-            sad = dataSnapshot.child("슬픔").getValue().toString();
-            angry = dataSnapshot.child("짜증").getValue().toString();
-            eh.setText(happy);
-            efa.setText(fireangry);
-            eda.setText(disappear);
-            es.setText(sad);
-            ea.setText(angry);
+            try{
+                happy = dataSnapshot.child(today).child("기쁨").getValue().toString();
+                fireangry = dataSnapshot.child(today).child("분노").getValue().toString();
+                disappear = dataSnapshot.child(today).child("불안").getValue().toString();
+                sad = dataSnapshot.child(today).child("슬픔").getValue().toString();
+                angry = dataSnapshot.child(today).child("짜증").getValue().toString();
+                eh.setText(happy);
+                efa.setText(fireangry);
+                eda.setText(disappear);
+                es.setText(sad);
+                ea.setText(angry);
+            }catch(Exception a){
+                childUpdates.put(today +"/기쁨", 0);
+                mDB_ref.updateChildren(childUpdates);
+                childUpdates.put(today +"/분노", 0);
+                mDB_ref.updateChildren(childUpdates);
+                childUpdates.put(today +"/불안", 0);
+                mDB_ref.updateChildren(childUpdates);
+                childUpdates.put(today +"/슬픔", 0);
+                mDB_ref.updateChildren(childUpdates);
+                childUpdates.put(today +"/짜증", 0);
+                mDB_ref.updateChildren(childUpdates);
+                eh.setText("0");
+                efa.setText("0");
+                eda.setText("0");
+                es.setText("0");
+                ea.setText("0");
+            }
         }
 
         @Override
-        public void onCancelled(@NonNull DatabaseError databaseError) {}
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+            Toast.makeText(getApplicationContext(),"인터넷 연결을 확인해 주세요.",Toast.LENGTH_LONG).show();
+        }
 
     };
 
 }
-
--- app/src/main/java/com/example/moodcoin/MainActivity.java
+++ app/src/main/java/com/example/moodcoin/MainActivity.java
@@ -1,67 +1,92 @@
 package com.example.moodcoin;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
-public class MainActivity extends Activity {
-    Button btn1;
+public class MainActivity extends AppCompatActivity {
+
+    Button login;
+    String id, today;
+    EditText ed_id;
+    Date currentTime;
+    String str = "";
+    int cnt=0;
+    SimpleDateFormat Format = new SimpleDateFormat("yyyyMMdd", Locale.getDefault());
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn1 = (Button)findViewById(R.id.startbtn);
-        String str = "";
-        String sstr = "";
-        ////////////////////////////////////////////////////////////////////////////////////////////////
-        Log.d("ㅇㅇㅇ", "test1");
+        login = (Button)findViewById(R.id.login);
+        ed_id = (EditText)findViewById(R.id.id_input);
+
+        //현재 날짜를 받아옴
+        currentTime  = Calendar.getInstance().getTime();
+        today = Format.format(currentTime);
+
+        //파일 입출력으로 check.txt의 내용을 불러옴
         try{
             BufferedReader br = new BufferedReader(new FileReader(getFilesDir()+"check.txt"));
             str = br.readLine();
             br.close();
-            Log.d("ㅇㅇㅇ", "test5" + str);
         }
         catch (IOException e) {
             e.printStackTrace();
         }
-        Log.d("ㅇㅇㅇ", "test4");
-        //str = str.trim();
-        Log.d("ㅇㅇㅇ", "test3");
-        Log.d("0000", "아아아"+str);
-        if(str.equals("O")){
-            Log.d("ㅇㅇㅇ", "점프");
+
+        //이전에 로그인 시, 로그인 창 없이 바로 LobbyActivity로 넘어감
+        if(str.length()==18){
+            cnt = 1;
             nextjump();
-            finish();
         }
-        ////////////////////////////////////////////////////////////////////////////////////////////////
-        btn1.setOnClickListener(new View.OnClickListener() {
+
+        // 디코 ID 입력 후 로그인 버튼을 누를 시
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                try{
-                    BufferedWriter bw = new BufferedWriter(new FileWriter(getFilesDir() + "check.txt", false));
-                    bw.write("O");
-                    bw.close();
-                }catch (Exception e){
-                    e.printStackTrace();
+            public void onClick(View view) {
+                if(ed_id.getText().toString().length()<18 || ed_id.getText().toString().length()>18)
+                    Toast.makeText(getApplicationContext(),"ID는 숫자 18자입니다.",Toast.LENGTH_LONG).show();
+                else{
+                    try{
+                        id = ed_id.getText().toString();
+                        BufferedWriter bw = new BufferedWriter(new FileWriter(getFilesDir() + "check.txt", false));
+                        bw.write(id);
+                        bw.close();
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                    nextjump();
                 }
-               nextjump();
             }
         });
-
     }
 
+    //LobbyActivity로 보내주는 인스턴스
     private void nextjump() {
-        Intent intent1 = new Intent(this, lobbyActivity.class);
+        Intent intent1 = new Intent(this, LobbyActivity.class);
+        if(cnt == 1)
+            intent1.putExtra("id", str);
+        else
+            intent1.putExtra("id", id);
+        intent1.putExtra("today", today);
         startActivity(intent1);
+        finish();
     }
 }
