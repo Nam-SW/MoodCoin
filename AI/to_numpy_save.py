e88a5560bb8f7fe6513fcd624749b944ad24cc88
Nam-Seung-Woo <45066060+Nam-SW@users.noreply.github.com>
2019-12-28 21:16:42 +0900

    '오류고침'

--- AI/to_numpy_save.py
+++ AI/to_numpy_save.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from AI_API import MoodCoin
+import pickle
 
 data = pd.read_excel('dataset/dataset.xlsx')
 # print(len(data['X_data']))
@@ -13,19 +14,31 @@ data = pd.read_excel('dataset/dataset.xlsx')
 data = data.dropna(how='any')
 
 mc = MoodCoin()
-X = np.array(mc.prepocessing(list(data['X_data'])))
-Y = data['Y_data'].to_numpy()
+X = [list(i) for i in mc.prepocessing(list(data['X_data']))]
+Y = list(data['Y_data'])
 
+print(len(X), len(Y))
+idx = Y.index('  ')
+X.pop(idx)
+Y.pop(idx)
 print(len(X), len(Y))
 # t = list(Y)
 # for i in range(6):
 #     print(t.count(i), end=' ')
-np.save('dataset/X_data.npy', X, allow_pickle=True)
-np.save('dataset/Y_data.npy', Y, allow_pickle=True)
+# np.save('dataset/X_data.npy', X, allow_pickle=True)
+# np.save('dataset/Y_data.npy', Y, allow_pickle=True)
+with open('dataset/x_data.pickle', 'wb') as f:
+    pickle.dump(X, f)
+with open('dataset/y_data.pickle', 'wb') as f:
+    pickle.dump(Y, f)
 
 # x = np.load('dataset/X_data.npy')
-x = np.load('dataset/X_data.npy')
-y = np.load('dataset/Y_data.npy')
+# y = np.load('dataset/Y_data.npy')
+
+with open('dataset/x_data.pickle', 'rb') as f:
+    x = pickle.load(f)
+with open('dataset/y_data.pickle', 'rb') as f:
+    y = pickle.load(f)
 
 print(x[:5])
 print(y[:5])
