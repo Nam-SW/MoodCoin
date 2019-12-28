62d1d112dd1bdac7463b27d1271d6a15c991f956
Nam-Seung-Woo <45066060+Nam-SW@users.noreply.github.com>
2019-12-28 21:30:23 +0900

    Update app.py

--- Server/app.py
+++ Server/app.py
@@ -20,12 +20,15 @@ def chat_update():
     talk = data['talk']
     print(id + ': ' + talk)
 
-    # pred = moodcoin.predict(talk)
-    # if pred == 0: # 대화한 내용이 뜻이 없는 업무적 내용 등일 경우
-    #     return
+    pred = moodcoin.predict(talk)[0]
+    if pred == 0: # 대화한 내용이 뜻이 없는 업무적 내용 등일 경우
+        return ''
+
 
     database = db.reference(id).get()
-    today = max(database.keys()) # 가장 최근 날짜
+    keys = list(database.keys())
+    keys.remove('friends')
+    today = max(keys) # 가장 최근 날짜
     database[today][emotion[1]] += 1
     db.reference(id).child(today).set(database[today])
     return ''
