27acebd0a4aac54ef11a6f940b0ee8e904efc590
hangyeol0531 <41174265+hangyeol0531@users.noreply.github.com>
2019-12-30 16:32:17 +0900

    netdb

--- app/src/main/java/com/example/moodcoin/FragmentMain.java
+++ app/src/main/java/com/example/moodcoin/FragmentMain.java
@@ -3,6 +3,7 @@ package com.example.moodcoin;
 import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -33,6 +34,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -40,6 +43,13 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -62,6 +72,8 @@ public class FragmentMain extends Fragment {
     SimpleDateFormat hour = new SimpleDateFormat("HH");
     SimpleDateFormat min = new SimpleDateFormat("mm");
     SimpleDateFormat date = new SimpleDateFormat("yyyyMMdd");
+    int flagnum = 1;
+    String addprice = " ";
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         childUpdates = new HashMap<>();
@@ -101,6 +113,7 @@ public class FragmentMain extends Fragment {
         a1 = (LinearLayout)v.findViewById(R.id.a1);
         a2 = (LinearLayout)v.findViewById(R.id.a2);
         a3 = (LinearLayout)v.findViewById(R.id.a3);
+        tv1 = (TextView)v.findViewById(R.id.tv1);
 
         h1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -247,6 +260,12 @@ public class FragmentMain extends Fragment {
         price = (TextView)v.findViewById(R.id.price);
 
         pieChart = (PieChart)v.findViewById(R.id.piechart);
+
+        addprice = price.getText().toString();
+        flagnum = 1;
+        new JSONTask().execute("http://10.120.72.146:3000/statemoney");
+        flagnum = 2;
+        new JSONTask().execute("http://10.120.72.146:3000/mywallet");
 
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
@@ -571,4 +590,92 @@ public class FragmentMain extends Fragment {
         }
     }
 
+    public class JSONTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                //JSONObject를 만들고 key value 형식으로 값을 저장해준다.
+                JSONObject jsonObject = new JSONObject();
+                if(flagnum == 1){
+                    jsonObject.accumulate("sendperson", id);
+                    jsonObject.accumulate("addprice", addprice);
+
+                }else if(flagnum == 2){
+                    jsonObject.accumulate("sendperson", id);
+                }
+                HttpURLConnection con = null;
+                BufferedReader reader = null;
+                Log.d("asdsad", jsonObject.toString());
+                try{
+                    URL url = new URL("http://10.120.72.146:3000/statemoney"); //주소
+                    if(flagnum == 2)  url = new URL("http://10.120.72.146:3000/mywallet");
+                    //URL url = new URL(urls[0]);
+                    //연결을 함
+                    con = (HttpURLConnection) url.openConnection();
+
+                    con.setRequestMethod("POST");//POST방식으로 보냄
+                    con.setRequestProperty("Cache-Control", "no-cache");//캐시 설정
+                    con.setRequestProperty("Content-Type", "application/json");//application JSON 형식으로 전송
+
+
+                    con.setRequestProperty("Accept", "text/html");//서버에 response 데이터를 html로 받음
+                    con.setDoOutput(true);//Outstream으로 post 데이터를 넘겨주겠다는 의미
+                    con.setDoInput(true);//Inputstream으로 서버로부터 응답을 받겠다는 의미
+                    con.connect();
+
+                    //서버로 보내기위해서 스트림 만듬
+                    OutputStream outStream = con.getOutputStream();
+                    //버퍼를 생성하고 넣음
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outStream));
+                    writer.write(jsonObject.toString());
+                    writer.flush();
+                    writer.close();//버퍼를 받아줌
+
+                    //서버로 부터 데이터를 받음
+                    InputStream stream = con.getInputStream();
+
+                    reader = new BufferedReader(new InputStreamReader(stream));
+
+                    StringBuffer buffer = new StringBuffer();
+
+                    String line = "";
+                    while((line = reader.readLine()) != null){
+                        buffer.append(line);
+                    }
+
+                    return buffer.toString();//서버로 부터 받은 값을 리턴해줌 아마 OK!!가 들어올것임
+
+                } catch (MalformedURLException e){
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if(con != null){
+                        con.disconnect();
+                    }
+                    try {
+                        if(reader != null){
+                            reader.close();//버퍼를 닫아줌
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if(flagnum == 1){
+
+            }else if(flagnum == 2){
+                tv1.setText(result);
+            }
+        }
+    }
 }
