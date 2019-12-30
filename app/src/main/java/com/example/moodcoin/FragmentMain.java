5349ba7797f8c08ebd0bedb71faa12468c62c2d1
yoongkim <dl2qja@gmail.com>
2019-12-30 18:22:33 +0900

    Update FragmentMain.java

--- app/src/main/java/com/example/moodcoin/FragmentMain.java
+++ app/src/main/java/com/example/moodcoin/FragmentMain.java
@@ -689,7 +689,9 @@ public class FragmentMain extends Fragment {
             if(flagnum == 1){
                 tv1.setText(result);
                 flagnum = 2;
-                new JSONTask().execute("http://10.120.72.146:3000/statemoney");
+
+                if(!getRecentDay().equals(today))
+                    new JSONTask().execute("http://10.120.72.146:3000/statemoney");
 
             } else if(flagnum == 2){
                 tv1.setText(result);
